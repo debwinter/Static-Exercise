d3e012da8688120d37fb0f0b0f040de04f4048c9
Debra Winter <deb@debwinter.com>
2024-01-25 16:03:15 -0500

    exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,86 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Are you American? (enter 'Y' for yes or 'N' for no): ");
+            char input1 = ValidateInput(UserInput(), 'Y', 'y', 'N', 'n');
+            Console.WriteLine();
+
+            if (input1 == 'Y' || input1 == 'y')
+            {
+                Console.Write("Enter the temperature in degrees Fahrenheit: ");
+                double temp = UserDouble();
+                Console.WriteLine();
+                Console.WriteLine($"The temperature in Celsius is: {TempConverter.FahrenheitToCelsius(temp)}");
+            }
+            else
+            {
+                Console.Write("Enter the temperature in degrees Celsius: ");
+                double temp = UserDouble();
+                Console.WriteLine();
+                Console.WriteLine($"The temperature in Fahrenheit is: {TempConverter.CelsiusToFahrenheit(temp)}");
+            }
+        }
+
+        static char UserInput()
+        {
+            bool isChar = char.TryParse(Console.ReadLine(), out char userInput);
+            while (!isChar)
+            {
+                Console.WriteLine("I'm sorry, I don't understand. Please try again: ");
+                isChar = char.TryParse(Console.ReadLine(), out userInput);
+            }
+            return userInput;
+        }
+
+        static char ValidateInput(char entry, char a, char b)
+        {
+            bool isValidEntry = entry == a || entry == b;
+            if (isValidEntry)
+            {
+                return entry;
+            }
+            else
+            {
+                char userInput;
+                do
+                {
+                    Console.WriteLine("Invalid entry. Please try again: ");
+                    userInput = UserInput();
+                    isValidEntry = userInput == a || userInput == b;
+                } while (!isValidEntry);
+                return userInput;
+            }
+        }
+
+        static char ValidateInput(char entry, char a, char b, char c, char d)
+        {
+            bool isValidEntry = entry == a || entry == b || entry == c || entry == d;
+            if (isValidEntry)
+            {
+                return entry;
+            }
+            else
+            {
+                char userInput;
+                do
+                {
+                    Console.WriteLine("Invalid entry. Please try again: ");
+                    userInput = UserInput();
+                    isValidEntry = userInput == a || userInput == b || userInput == c || userInput == d;
+                } while (!isValidEntry);
+                return userInput;
+            }
+        }
+
+        static double UserDouble()
+        {
+            bool isDouble = double.TryParse(Console.ReadLine(), out double userInput);
+            while (!isDouble)
+            {
+                Console.WriteLine("I'm sorry, I don't understand. Please try again: ");
+                isDouble = double.TryParse(Console.ReadLine(), out userInput);
+            }
+            return userInput;
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		static TempConverter()
+		{
+		}
+
+		public static double FahrenheitToCelsius(double f)
+		{
+			return (f - 32) / 1.8;
+		}
+
+		public static double CelsiusToFahrenheit(double c)
+		{
+			return (c * 9 / 5) + 32;
+		}
+	}
+}
+
